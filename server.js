626e8f972d377d9d8bb6ee023452da1659177826
tassioalves <viniciusalves.tva@gmail.com>
2020-11-13 18:00:23 -0400

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const config = require('config');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').createServer(app);
+
+const PORT = process.env.PORT || config.get('serverPort');
+
+/**
+ * Middlewares
+ */
+app.use(cors());
+app.use(express.json({extended: false}));
+
+/**
+ * Routes
+ */
+
+/**
+ * Application start
+ */
+server.listen(PORT, ()=>{
+    console.log('HTTP Server listening');
+});
