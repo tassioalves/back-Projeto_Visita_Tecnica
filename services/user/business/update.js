6c7ba538800da77dc76461ed74cf43384f3cc3bf
Gustavo Y. Maruyama <gustavo.maruyama@ifms.edu.br>
2021-03-29 09:42:28 -0400

    update de usuario corrigido

--- services/user/business/update.js
+++ services/user/business/update.js
@@ -1,11 +1,9 @@
 const User = require('../model/User');
 const error = require('../../../utils/error');
 
-module.exports = async (data) =>{
-    const user = await User.findByIdAndUpdate(data._id, data, {new: true});
-
+module.exports = async (user, data) =>{
+    await User.findByIdAndUpdate(user._id, data, {new: true});
     if(!user){
         throw await error([{msg: 'Usuario não encontrado na base de dados.'}])
     }
-
 }
